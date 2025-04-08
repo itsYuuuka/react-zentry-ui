853d0dd924af38cc31b7a3f9c9597d230f332172
itsYuuuka <okan_altun@hotmail.com>
2025-04-09 01:37:03 +0200

    Refactor styles and update Tailwind configuration for improved design consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome to React + TypeScript + Vite</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Welcome to React + TypeScript + Vite</h1>
     </main>
   )
 }

a0dc66a642c4fe8f3612d349e7459ee53032e002
itsYuuuka <okan_altun@hotmail.com>
2025-04-09 00:59:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">Welcome to React + TypeScript + Vite</h1>
+    </main>
+  )
+}
+
+export default App

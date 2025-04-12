926fb768d2521140dcc7b8b1a2d70e30327a45e6
itsYuuuka <okan_altun@hotmail.com>
2025-04-12 17:59:37 +0200

    Add Button component and update Hero with enhanced title display

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -93,6 +93,9 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>a</b>ming
+      </h1>
     </div>
   );
 };

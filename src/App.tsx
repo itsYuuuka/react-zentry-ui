ff00010cff8f0d21d991402fe5190224d911c5d0
itsYuuuka <okan_altun@hotmail.com>
2025-04-13 16:12:34 +0200

    Fix import statements and formatting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import About from "./components/About"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
+import About from "./components/About";
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Features from "./components/Features";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
-      <Hero />  
+      <Hero />
       <About />
+      <Features />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;

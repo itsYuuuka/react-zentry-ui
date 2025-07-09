b216985b5d60603adac58acce21ee4e459717759
itsYuuuka <okan_altun@hotmail.com>
2025-07-09 16:10:23 +0200

    remove explicit JSX return types

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,16 +5,13 @@ interface ImageClipBoxProps {
   clipClass?: string;
 }
 
-const ImageClipBox = ({
-  src,
-  clipClass = "",
-}: ImageClipBoxProps): JSX.Element => (
+const ImageClipBox = ({ src, clipClass = "" }: ImageClipBoxProps) => (
   <div className={clipClass}>
     <img src={src} alt="" />
   </div>
 );
 
-const Contact = (): JSX.Element => (
+const Contact = () => (
   <div id="contact" className="my-20 min-h-96 w-screen px-10">
     <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
       <div className="absolute -left-20 top-0 hidden h-full w-72 overflow-hidden sm:block lg:left-20 lg:w-96">
